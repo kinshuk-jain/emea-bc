6e88b9c14bc12426a32b4e2ccd41feedb9461623
kinshuk.jain <kinshuk.jain@klarna.com>
2021-02-27 18:51:43 +0100

    Improve server performance and add book detail api

--- server/index.js
+++ server/index.js
@@ -4,18 +4,36 @@ const csv = require('csvtojson');
 const cors = require('cors');
 
 const server = express();
+let jsonBooks = [];
 
 //accept only JSON
 server.use(bodyParser.json());
 server.use(cors());
 
 // healthcheck API
-server.get('/api/ping', (req, res) => res.send('pong'));
+server.get('/ping', (req, res) => res.send('pong'));
 
+// Note: Could be paginated. Not paginating for now
 server.get('/api/books', async (req, res) => {
-  const jsonBooks = await csv().fromFile('./books.csv');
   res.send(jsonBooks);
 });
 
-//set port and log to the console
-server.listen(3000, () => console.log('server listening'));
+server.get('/api/books/:id', async (req, res) => {
+  const bookId = parseInt(req.params.id);
+
+  if (isNaN(bookId) || bookId < 0 || bookId >= jsonBooks.length) {
+    return res.status(400).send({
+      error: 'Invalid book id',
+    });
+  }
+  res.send(jsonBooks[bookId]);
+});
+
+// Note: Not checking if dataset can fit in memory
+csv()
+  .fromFile('./books.csv')
+  .then(result => {
+    jsonBooks = result;
+    //set port and log to the console
+    server.listen(3000, () => console.log('server listening'));
+  });
